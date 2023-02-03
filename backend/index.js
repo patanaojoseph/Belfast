80102101a285989e5f03752a7a12d3439b0f51a7
patanaojoseph <97107931+patanaojoseph@users.noreply.github.com>
2023-02-04 01:03:23 +0800

    get products from dbase

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,17 @@ const db = mysql.createConnection({
   password: "",
   database: "belfast_db",
 });
-
+// to test if the backend is working
 app.get("/", (req, res) => {
   res.json("This is the backend");
+});
+// get data from mysql
+app.get("/products", (req, res) => {
+  const q = "SELECT * from products";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
 app.listen(5000, () => {
